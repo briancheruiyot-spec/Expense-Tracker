de6b84a14c13d0a59572b4655313904d18911237
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-04-14 12:05:43 +0300

    Updated ExpensesTable

---
+++ src/ExpenseTable.jsx
@@ -0,0 +1,15 @@
+const ExpenseTable = ({expenses}) => {
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+export default ExpenseTable
